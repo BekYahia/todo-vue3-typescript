09ed251b161137948cb50e201db2a5561798b4ed
BekYahia <albakrieee@gmail.com>
2021-05-21 13:57:06 +0200

    production prep

--- src/services/todos.ts
+++ src/services/todos.ts
@@ -1,10 +1,15 @@
 import axios from 'axios'
+
+const url = process.env.NODE_ENV === 'production'
+	? 'https://jsonplaceholder.typicode.com'
+	: ''
+
 export default {
 	getTodos(){
-		return axios.get('todos')
+		return axios.get(`${url}/todos`)
 	},
 
 	getTodo(id: number) {
-		return axios.get(`/todos/${id}`)
+		return axios.get(`${url}/todos/${id}`)
 	}
 }
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
 	lintOnSave: false,
 	devServer: {
 		proxy: 'https://jsonplaceholder.typicode.com/'
-	}
+	},
+	publicPath: process.env.NODE_ENV === 'production'
+		? '/todo-vue3-typescript/'
+		: '/'
   };
